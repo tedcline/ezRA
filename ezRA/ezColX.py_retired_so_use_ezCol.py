978b32d5a9e68b694bd370f47c708101d4b45361
Ted Cline <tedClineGit@gmail.com>
2023-02-07 16:49:07 -0700

    ezColX.py retired

---
+++ ezRA/ezColX.py_retired_so_use_ezCol.py
@@ -0,0 +1,20 @@
+
+# ezColX.py_retired_so_use_ezCol.py
+# Feb-7-2023
+
+
+# ezCol221110a.py was the last single-process version.
+
+# Then with Todd Ullery's help, the experimental ezColX230205c.py was developed
+# to explore a program with 2 processes, to improve dashboard responsiveness.
+
+# The experiment was a success.
+
+# The last ezColX230205c.py is now named ezCol230206a.py,
+# and the short-lived experimental name ezColX is retired.
+
+# Stop looking for ezColX.
+# Use the new ezCol.
+
+# ---
+
